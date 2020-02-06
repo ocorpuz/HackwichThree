b2cdeca446c25436eb5fc85428fef60b7f9dabec
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-02-06 10:51:24 -1000

    Completed Part 4

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 var FirstString = "The background will turn to Blue"
 var SecondString = "The background will turn to Green"
+var thirdrdstring = "Turn green"
+var fourthstring = "Turn purple"
+    
+    @IBOutlet weak var RedLabel: UILabel!
+    @IBOutlet weak var BlueLabel: UILabel!
+    
+    @IBOutlet weak var GreenLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,10 +29,21 @@ var SecondString = "The background will turn to Green"
         if FirstString == "The background color will turn red"
         {
             self.view.backgroundColor = UIColor.red
+            self.RedLabel.text = "red"
         }
         else
         {
             self.view.backgroundColor = UIColor.blue
+            self.BlueLabel.text = "blue"
+        }
+    }
+    
+    @IBAction func MagicBUttonPressed(_ sender: Any)
+    {
+        if thirdrdstring == "Turn green"
+        {
+            self.view.backgroundColor = UIColor.green
+            self.GreenLabel.text = "Green"
         }
     }
     
